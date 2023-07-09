26bc2fe4e680b9d7847a58f93b31ede025e76f9a
Gagan jain <129584007+Gaganjain68@users.noreply.github.com>
2023-07-09 18:22:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/harshjain883/FallenXMusic",
+    "https://github.com/Gaganjain68/FallenXmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
