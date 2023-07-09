69077dadc63b698fbba9e3840a9b48ded3710da1
Gagan jain <129584007+Gaganjain68@users.noreply.github.com>
2023-07-09 17:39:53 +0530

    Add files via upload

---
+++ FallenX/core/bot.py
@@ -0,0 +1,44 @@
+import sys
+
+from pyrogram import Client
+
+import config
+
+from ..logging import LOGGER
+
+
+class FallenXBot(Client):
+    def __init__(self):
+        LOGGER(__name__).info(f"Starting Bot...")
+        super().__init__(
+            "Fallen",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            bot_token=config.BOT_TOKEN,
+        )
+
+    async def start(self):
+        await super().start()
+        get_me = await self.get_me()
+        self.username = get_me.username
+        self.id = get_me.id
+        if get_me.last_name:
+            self.name = get_me.first_name + " " + get_me.last_name
+        else:
+            self.name = get_me.first_name
+        a = await self.get_chat_member(config.LOGGER_ID, self.id)
+        if a.status != "administrator":
+            LOGGER(__name__).error(
+                "Please promote Bot as Admin in Logger Group"
+            )
+            sys.exit()
+        LOGGER(__name__).info(f"MusicBot Started as {self.name}")
+        try:
+            await self.send_message(
+                config.LOGGER_ID, f"**㊥ {config.BOT_NAME} ㊥**\n\n𖢵 ɪᴅ : `{self.id}`\n𖢵 ɴᴀᴍᴇ : {self.name}\n𖢵 ᴜsᴇʀɴᴀᴍᴇ : @{self.username}"
+            )
+        except:
+            LOGGER(__name__).error(
+                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
+            )
+            sys.exit()
