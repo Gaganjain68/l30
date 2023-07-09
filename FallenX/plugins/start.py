8f22c5dd8d224e28491ba3111e1fb116bf79dbf6
Gagan jain <129584007+Gaganjain68@users.noreply.github.com>
2023-07-09 21:13:10 +0530

    Update start.py

--- FallenX/plugins/start.py
+++ FallenX/plugins/start.py
@@ -43,7 +43,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAxkBAAJE8GK4EsoLVZC2SW5W5Q-QAkaoN8f_AAL9BQACiy14VGoQxOCDfE1KKQQ")
+            await message.reply_sticker("CAACAgUAAxkBAAIsamRDh6gQvJYIH8NvBh4eRoyZoTlXAAJfBgACoVAgVfvBxYHodmfOLwQ")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"], reply_markup=keyboard
@@ -202,7 +202,7 @@ async def start_comm(client, message: Message, _):
         out = private_panel(_, app.username, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_sticker("CAACAgQAAx0CZIiVngABBB6tYvs-dV4rbsUv3h3oKbscOz07ofQAAkkMAAJLUMlT3uEIKksW8bwpBA")
+                await message.reply_sticker("CAACAgUAAxkBAAIsZmRDh0RwnHkj0z7zLLHJqfBvVZqgAAJrBgACtjMhVZpZivhJsHmhLwQ")
                 await message.reply_photo(
                     photo=config.START_IMG_URL,
                     caption=_["start_2"].format(
