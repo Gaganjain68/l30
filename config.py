88f17c2a8f4de4ba80724f1d734645337ea6ea62
Gagan jain <129584007+Gaganjain68@users.noreply.github.com>
2023-07-09 18:39:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/Gaganjain68/FallenXmusic",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
